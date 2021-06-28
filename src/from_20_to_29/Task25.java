b9389407939404d417eb18f83f24cfa41c6462c8
kekos <vladoren1@mail.ru>
2021-06-28 10:17:32 +0300

    Priority Queue Update

--- src/from_20_to_29/Task25.java
+++ src/from_20_to_29/Task25.java
@@ -2,6 +2,19 @@ package from_20_to_29;
 
 import java.util.*;
 
+/*
+There are a number of students in a school who wait to be served.
+Two types of events, ENTER and SERVED, can take place which are described below.
+ -> ENTER: A student with some priority enters the queue to be served.
+ -> SERVED: The student with the highest priority is served (removed) from the queue.
+A unique id is assigned to each student entering the queue.
+The queue serves the students based on the following criteria (priority criteria):
+ -> The student having the highest Cumulative Grade Point Average (CGPA) is served first.
+ -> Any students having the same CGPA will be served by name in ascending case-sensitive alphabetical order.
+ -> Any students having the same CGPA and name will be served in ascending order of the id.
+The locked stub code prints the names of the students yet to be served in the priority order.
+If there are no such student, then the code prints EMPTY.
+*/
 
 
 public class Task25 {
