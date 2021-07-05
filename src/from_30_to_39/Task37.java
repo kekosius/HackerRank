4004c96d826b5765c14d5eb9a083e452c5b6dda5
kekos <vladoren1@mail.ru>
2021-07-05 14:40:30 +0300

    Optimized solution with HashSet

--- src/from_30_to_39/Task37.java
+++ src/from_30_to_39/Task37.java
@@ -2,43 +2,37 @@ package from_30_to_39;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.HashSet;
 import java.util.Scanner;
+
+/*
+In this problem, you are given n integers.
+You need to find the maximum number of unique integers among all the possible contiguous subarrays of size m.
+*/
 
 public class Task37 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         int m = input.nextInt();
-        int num;
-        int c = 0;
         int first;
         int last;
-        int max;
+        int max = Integer.MIN_VALUE;
 
         Deque<Integer> deque = new ArrayDeque<>();
+        HashSet<Integer> unique = new HashSet<>();
 
-        for (int i = 0; i < m; i++) {
-            num = input.nextInt();
-            c = c + uniqueCounter(deque, num);
-            deque.add(num);
-        }
-        max = c;
-        for (int i = m; i < n; i++) {
-            if (max == m) break;
-            first = deque.pollFirst();
-            c = c - uniqueCounter(deque, first);
+        for (int i = 0; i < n; i++) {
+            if (i >= m) {
+                if (max == m) break;
+                first = deque.pollFirst();
+                if (!deque.contains(first)) unique.remove(first);
+            }
             last = input.nextInt();
-            c = c + uniqueCounter(deque, last);
             deque.add(last);
-            if (c > max) max = c;
+            unique.add(last);
+            if (unique.size() > max) max = unique.size();
         }
-        System.out.println(c);
-    }
-    public static int uniqueCounter(Deque<Integer> deque, int num){
-
-        for (Integer x: deque){
-            if (x == num) return 0;
-        }
-        return 1;
+        System.out.println(max);
     }
 }
